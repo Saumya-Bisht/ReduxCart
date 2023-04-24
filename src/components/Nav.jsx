6888a9083574882c5e81ee8745b3c63a2c6916b3
Saumya-Bisht <saumyabisht05@gmail.com>
2023-04-24 15:48:34 +0530

    reduxcart

---
+++ src/components/Nav.jsx
@@ -0,0 +1,35 @@
+
+import './Nav.css'
+import {Link} from 'react-router-dom'
+import { useSelector } from 'react-redux';
+// import Login from './Login';
+
+
+
+function Nav(props) {
+    const Cart=useSelector((state)=> state.cart)
+     console.log(Cart);
+  return (
+    <div className="nav">
+      <div id="title">
+        <h2>React Context</h2>
+      </div>
+      <div id="menu">
+        <Link to="/">Home</Link>
+        
+        <Link to="/products">Products</Link>
+        
+        <Link to="/cart">Cart <span>{Cart.length}</span> </Link>
+        <button style={{color:"white",backgroundColor:"green",border:"0",height:"25px",width:"100px",cursor:"pointer"}}>
+          Logout
+        </button>
+        <span  style={{ marginLeft: "30px", color: "yellow", fontSize: "22px" }}>
+          Welcome! 
+        
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
